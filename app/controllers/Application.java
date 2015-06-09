1106c9dcfb780548b720b4c22eec91a203242166
ThusharaUC <thusharadahanayaka@gmail.com>
2015-06-09 13:10:49 +0530

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("Test");
+        return ok("Test, meka pasange wadak,\n Ow, Ape Kassa Pasange wadak :P");
     }
 
 }
